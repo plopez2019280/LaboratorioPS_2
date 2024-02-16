61419388c405cbe4d2eec0d6ee086d035ce48d3f
Informatica <informatica@alumno.local>
2024-02-16 10:45:28 -0600

    Actualización de archivos

--- controllers/alumno.controller.js
+++ controllers/alumno.controller.js
@@ -1,26 +1,24 @@
-const { response, json } = require('express');
 const bcryptjs = require('bcryptjs');
-const Alumno = require('../models/alumno');
+const Student = require('../models/alumno');
 
-
-const alumnosGet = async (req, res = response) => {
+const alumnoGet = async(req, res = response) => {
     const { limite, desde } = req.query;
     const query = { estado: true };
 
-    const [total, alumnos] = await Promise.all([
+    const [total, student] = await Promise.all([
         Alumno.countDocuments(query),
         Alumno.find(query)
-            .skip(Number(desde))
-            .limit(Number(limite))
+        .skip(Number(desde))
+        .limit(Number(limite))
     ]);
 
     res.status(200).json({
         total,
-        alumnos
+        alumno
     });
 }
 
-const getAlumnoById = async (req, res) => {
+const getAlumnoById = async(req, res) => {
     const { id } = req.params;
     const alumno = await Alumno.findOne({ _id: id });
 
@@ -29,76 +27,52 @@ const getAlumnoById = async (req, res) => {
     });
 }
 
-const alumnosPut = async (req, res) => {
+const alumnoPut = async(req, res = response) => {
     const { id } = req.params;
-    const { _id, estado, ...resto } = req.body;
+    const { _id, password, role, ...resto } = req.body
 
-    await Alumno.findByIdAndUpdate(id, resto);
-
-    const alumno = await Alumno.findOne({ _id: id });
-
-    res.status(200).json({
-        msg: 'Alumno Actualizado exitosamente',
-        alumno
-    })
-}
-
-const alumnosDelete = async (req, res) => {
-    const { id } = req.params;
-    await Alumno.findByIdAndUpdate(id, { estado: false });
-
-    const alumno = await Alumno.findOne({ _id: id });
-
-    res.status(200).json({
-        msg: 'Alumno eliminado exitosamente',
-        alumno
-    });
-}
-
-/*const alumnosPost = async (req, res) => {
-    // Check if req.body exists
-    if (!req.body) {
-        return res.status(400).json({ error: "Request body is missing" });
+    if (password) {
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
     }
 
-    const { nombres, apellidos, password, carne, correo, role } = req.body;
+    const alumno = await Student.findByIdAndUpdate(id, resto);
 
-    // Check if required properties exist in req.body
-    if (!nombres || !apellidos || !password || !carne || !correo || !role) {
-        return res.status(400).json({ error: "Required properties are missing from request body" });
-    }
+    res.status(200).json({
+        msg: 'Estudiante Actualizado Exitosamente',
+        alumno
+    });
+}
 
-    const alumno = new Alumno({ nombres, apellidos, password, carne, correo, role });
+const alumnoDelete = async(req, res) => {
+    const { id } = req.params;
+    const alumno = await Alumno.findByIdAndUpdate(id, { estado: false });
+
+    res.status(200).json({
+        msg: 'Estudiante eliminado exitosamente',
+        alumno
+    });
+}
+
+const alumnoPost = async(req, res) => {
+    const { nombre, correo, password, curso, role } = req.body;
+    const student = new Student({ nombre, correo, password, curso, role });
 
     const salt = bcryptjs.genSaltSync();
+    console.log(password);
     alumno.password = bcryptjs.hashSync(password, salt);
 
     await alumno.save();
-    res.status(200).json({
-        alumno
-    });
-}*/
-
-
-const alumnosPost = async (req, res) => {
-    const { nombres, apellidos, password, carne, correo, role } = req.body;
-    const alumno = new Alumno({nombres,apellidos,password,carne,correo,role});
-
-    const salt = bcryptjs.genSaltSync();
-    alumno.password = bcryptjs.hashSync(password, salt);
-
-    await alumno.save();
-    res.status(200).json({
+    console.log({ nombre, correo, password, curso, role })
+    res.status(202).json({
         alumno
     });
 }
-
-
 
 module.exports = {
-    alumnosPost,
-    alumnosGet,
+    alumnoPost,
+    alumnoGet,
+    alumnoDelete,
     getAlumnoById,
-    alumnosPut,
-    alumnosDelete
+    alumnoPut
 }
--- models/alumno.js
+++ models/alumno.js
@@ -1,35 +1,34 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const AlumnoSchema = Schema ({
-    nombres: {
+const AlumnoSchema = Schema({
+    nombre: {
         type: String,
-        required: [true, 'El alumno debe de tener nombres']
-    },
-    apellidos: {
-        type: String,
-        required: [true, 'El alumno debe de tener apellidos']
-    },
-    password: {
-        type: String,
-        required: [true, 'El alumno debe de tener contraseña']
-    },
-    carne: {
-        type: String,
-        required: [true, 'El alumno debe de tener carné']
+        require: [true, 'El nombre debe de ser obligatorio']
     },
     correo: {
         type: String,
-        required: [true, 'El alumno debe de tener correo']
+        require: [true, 'el correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, 'la clave es obligatoria']
     },
     role: {
         type: String,
-        required: true,
-        enum: ["TEACHER_ROLE", "STUDENT_ROLE"]
+        enum: ["STUDENT_ROLE"]
     },
     estado: {
         type: Boolean,
         default: true
-    }
+    },
+
 });
+
+StudentSchema.methods.toJSON = function(){
+    const{ __v, password, _id, ...alumno} = this.toObject();
+    alumno.uid = _id;
+    return alumno;
+};
 
 module.exports = model('Alumno', AlumnoSchema);
